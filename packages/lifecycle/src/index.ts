dd1a4b8c2724b293ad3b68bcbbec8ef71ad89b61
popcoondev <popcoondev@gmail.com>
2026-09-18 09:37:38 +0900

    chore: prepare v0.5.0 release

--- packages/lifecycle/src/index.ts
+++ packages/lifecycle/src/index.ts
@@ -5,8 +5,8 @@ import { lifecycleValidator } from "./validate.js";
 
 export const lifecyclePlugin: ArchMapPlugin = {
   name: "@archmap/lifecycle",
-  version: "0.4.1",
-  requires: { "@archmap/core": "^0.4.1" },
+  version: "0.5.0",
+  requires: { "@archmap/core": "^0.5.0" },
   elementTypes: [...LIFECYCLE_ELEMENT_TYPES],
   relationTypes: [...LIFECYCLE_RELATION_TYPES],
   validators: [lifecycleValidator],
